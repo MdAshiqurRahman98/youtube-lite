b8de9d2fb609ca2d4d3850ee90add97370078ec1
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-04-04 07:57:09 +0600

    main.dart updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_lite/presentation/screen/home/home_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_lite/core/constants/app_colors.dart';
+import 'package:youtube_lite/core/constants/app_strings.dart';
+import 'package:youtube_lite/presentation/home/screen/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const YouTubeLiteApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class YouTubeLiteApp extends StatelessWidget {
+  const YouTubeLiteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'YouTube Lite',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: HomeScreen(),
+      title: AppStrings.appName,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.dark(
+          primary: AppColors.youtubePrimary,
+          surface: AppColors.surfaceDark,
+        ),
+        scaffoldBackgroundColor: AppColors.backgroundDark,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.backgroundDark,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+        splashColor: AppColors.youtubePrimary.withValues(alpha: 0.12),
+        highlightColor: AppColors.youtubePrimary.withValues(alpha: 0.06),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
     );
   }
 }
