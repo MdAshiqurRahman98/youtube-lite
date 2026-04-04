5ae1cb073e0742c803108250b86450121d0820f2
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-04-04 10:24:40 +0600

    youtube_screen updated

--- lib/presentation/youtube/screen/youtube_screen.dart
+++ lib/presentation/youtube/screen/youtube_screen.dart
@@ -3,11 +3,12 @@ import 'package:youtube_lite/presentation/core/widget/web_view_wrapper.dart';
 
 class YoutubeScreen extends StatelessWidget {
   const YoutubeScreen({super.key, required this.title, required this.url});
+
   final String title;
   final String url;
 
   @override
   Widget build(BuildContext context) {
-    return WebViewWrapper(url: url, title: title);
+    return WebViewWrapper(title: title, url: url);
   }
 }
